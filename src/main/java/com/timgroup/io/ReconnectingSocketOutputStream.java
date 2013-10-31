7e9f0a6831bcb64459c0b4ef2ddb1d19015dafe4
Tom Anderson <tom.anderson@timgroup.com>
2013-10-31 18:36:07 +0000

    A first cut at a stream which magically reconnects over the network

---
+++ src/main/java/com/timgroup/io/ReconnectingSocketOutputStream.java
@@ -0,0 +1,84 @@
+package com.timgroup.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ReconnectingSocketOutputStream extends OutputStream {
+
+    public static final int DEFAULT_RETRY_COUNT = 3;
+
+    private final String host;
+    private final int port;
+    private final int retryCount;
+    private Socket socket;
+    private OutputStream out;
+
+    public ReconnectingSocketOutputStream(String host, int port, int retryCount) throws IOException {
+        this.host = host;
+        this.port = port;
+        this.retryCount = retryCount;
+        connect();
+    }
+
+    public ReconnectingSocketOutputStream(String host, int port) throws IOException {
+        this(host, port, DEFAULT_RETRY_COUNT);
+    }
+
+    private void connect() throws IOException {
+        if (socket != null) {
+            throw new IllegalStateException("already connected to " + socket);
+        }
+
+        Socket socket = new Socket(host, port);
+        socket.shutdownInput();
+        socket.setKeepAlive(true); // might help
+        OutputStream out = socket.getOutputStream();
+
+        // do this atomicallyish, so we can never have socket not null but out null
+        this.socket = socket;
+        this.out = out;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        write(new byte[] {(byte) b});
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        for (int i = 0; i < retryCount; ++i) {
+            if (socket == null || (!socket.isConnected()) || socket.isClosed()) {
+                closeQuietly();
+                connect();
+            }
+            try {
+                out.write(b, off, len);
+                out.flush();
+                return;
+            } catch (Exception e) {
+                closeQuietly();
+            }
+        }
+    }
+
+    public void closeQuietly() {
+        try {
+            close();
+        } catch (IOException e) {}
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (socket == null) {
+            return;
+        }
+        try {
+            socket.close();
+        } finally {
+            socket = null;
+            out = null;
+        }
+    }
+
+}
---
+++ src/test/java/com/timgroup/io/ReconnectingSocketOutputStreamTest.java
@@ -0,0 +1,105 @@
+package com.timgroup.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ReconnectingSocketOutputStreamTest {
+
+    private static final int MINIMUM_DATA_TO_GET_BROKEN_PIPE = 128 * 1024 + 1;
+    private static final Random RANDOM = new Random();
+
+    @Rule
+    public final TestRule timeout = new Timeout(1000);
+
+    private final int port = 1024 + RANDOM.nextInt(65536 - 1024);
+    private ServerSocket serverSocket;
+
+    @Before
+    public void closeServerSocket() throws IOException {
+        if (serverSocket != null) {
+            serverSocket.close();
+        }
+    }
+
+    @Test
+    public void reportsFailureToConnect() throws Exception {
+        try {
+            new ReconnectingSocketOutputStream("localhost", port);
+            fail("should have thrown ConnectException");
+        } catch (ConnectException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void writesBytesToASocket() throws Exception {
+        serverSocket = new ServerSocket(port);
+
+        Future<String> line = readLine(serverSocket);
+
+        ReconnectingSocketOutputStream out = new ReconnectingSocketOutputStream("localhost", port);
+        out.write("hello\n".getBytes());
+        out.close();
+
+        assertEquals("hello", line.get(100, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void reconnectsIfNecessaryOnWrite() throws Exception {
+        serverSocket = new ServerSocket(port);
+
+        Future<Socket> socket = connect(serverSocket);
+
+        ReconnectingSocketOutputStream out = new ReconnectingSocketOutputStream("localhost", port);
+
+        socket.get(100, TimeUnit.MILLISECONDS).close();
+        Future<String> line = readLine(serverSocket);
+
+        out.write(ByteArrayUtils.pad("hello\n".getBytes(), MINIMUM_DATA_TO_GET_BROKEN_PIPE));
+        out.close();
+
+        assertEquals("hello", line.get(100, TimeUnit.MILLISECONDS));
+    }
+
+    private Future<String> readLine(final ServerSocket serverSocket) {
+        return Executors.newSingleThreadExecutor().submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Socket socket = serverSocket.accept();
+                try {
+                    return new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
+                } finally {
+                    socket.close();
+                }
+            }
+        });
+    }
+
+    private Future<Socket> connect(final ServerSocket serverSocket) {
+        return Executors.newSingleThreadExecutor().submit(new Callable<Socket>() {
+            @Override
+            public Socket call() throws Exception {
+                return serverSocket.accept();
+            }
+        });
+    }
+
+}
